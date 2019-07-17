6ea3afe22f080f3ef9c2a0886ba1d5e0da4cab0d
Diana Milena Arévalo Zárate <50525955+dmarevaloz@users.noreply.github.com>
2019-07-17 09:29:44 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,12 @@
 ## jjj,18
 ##
 ##
+mail=open('data.csv','r').readlines()
+mail=[row.replace('\n','').split('\t') for row in mail]
+b=[row[4].split(',') for row in mail]
+n=','.join([row[4] for row in mail]).split(',')
+n=[row.replace('\n','').split(':') for row in n]
+n
+mod=([row[0] for row in n])
+for i in sorted(set(mod)):
+  print(f"{i},{mod.count(i)}")
