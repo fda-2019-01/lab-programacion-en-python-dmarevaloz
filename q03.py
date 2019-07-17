c79d61a0f5b806ce656ceba2b6fd8389fa1b0d4d
Diana Milena Arévalo Zárate <50525955+dmarevaloz@users.noreply.github.com>
2019-07-17 09:23:53 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,12 @@
 ## D,23
 ## E,67
 ##
+mail=open('data.csv','r').readlines()
+mail=[row.split('\t') for row in mail]
+a=dict()
+for row in mail: 
+  if row[0] not in a.keys():
+    a[row[0]]=0
+  a[row[0]]=a[row[0]]+int(row[1])
+for i in sorted(a.keys()):
+  print(i,',',a[i],sep='')
