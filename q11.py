47e697bebd9372a8896c5f90a5c0b6df19a46ae7
Diana Milena Arévalo Zárate <50525955+dmarevaloz@users.noreply.github.com>
2019-07-17 09:30:30 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -11,3 +11,8 @@
 ## E,2,3
 ## E,3,3
 ##
+mail=open('data.csv','r').readlines()
+mail=[row.replace('\n','').split('\t') for row in mail]
+mail=[[row[0], row[3].split(','), row[4].split(',')] for row in mail]
+for i in mail: 
+  print(f'{i[0]},{len(i[1])},{len(i[2])}')
