4aabab2cc4dab7cbda137c428a8c16e5466ddd21
Diana Milena Arévalo Zárate <50525955+dmarevaloz@users.noreply.github.com>
2019-07-17 09:31:21 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -10,3 +10,14 @@
 ## f,110
 ## g,35
 ##
+mail=open('data.csv','r').readlines()
+mail=[row.split('\t') for row in mail]
+mail=[[row[3].split(','), row[1]] for row in mail]
+a=dict()
+for row in mail: 
+  for k in range(len(row[0])):
+    if row[0][int(k)] not in a.keys():
+      a[row[0][int(k)]]=0
+    a[row[0][int(k)]]=a[row[0][int(k)]]+int(row[1])
+for i in sorted(a.keys()):
+  print(i,',',a[i],sep='')
